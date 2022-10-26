39f3c975715785130a876e0fea1e2fe2f1730451
Arthur MICHEL <arthur.michel@capgemini.com>
2022-10-26 18:08:04 +0200

    add color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { SquareBookmarkComponent } from './components/square-bookmark/square-bookmark.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DragDropModule} from '@angular/cdk/drag-drop';
+import { AddbookmarkComponent } from './components/addbookmark/addbookmark.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SquareBookmarkComponent
+    SquareBookmarkComponent,
+    AddbookmarkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/addbookmark/addbookmark.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addbookmark',
+  templateUrl: './addbookmark.component.html',
+  styleUrls: ['./addbookmark.component.scss']
+})
+export class AddbookmarkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addBookmark(){
+    
+  }
+
+}
