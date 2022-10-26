f0fa77af29b9a52b0aeb2d2cd8159437e73f78e7
Arthur MICHEL <arthur.michel@capgemini.com>
2022-10-26 16:53:58 +0200

    Add Local Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SquareBookmarkComponent } from './components/square-bookmark/square-bookmark.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { SquareBookmarkComponent } from './components/square-bookmark/square-boo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
